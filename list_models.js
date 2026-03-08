4be1094dfdc580377b0c86ce9b413b4750532caf
İbrahim Öner <ibrahimoner@192.168.1.20>
2026-03-09 01:36:28 +0300

    chore: major cleanup - removed unused PDF/Groq/temporary files and updated dependencies

--- list_models.js
+++
@@ -1,8 +0,0 @@
-const { GoogleGenerativeAI } = require('@google/generative-ai');
-const genAI = new GoogleGenerativeAI('AIzaSyDugtkFoFl4n_Iww6RIC2GL1r9T5WgsQqg');
-async function run() {
-  const response = await fetch('https://generativelanguage.googleapis.com/v1beta/models?key=AIzaSyDugtkFoFl4n_Iww6RIC2GL1r9T5WgsQqg');
-  const data = await response.json();
-  console.log(data.models.map(m => m.name).join('\n'));
-}
-run();
